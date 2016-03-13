eed8c82a397dfbc72e96b077a1d47da5da3671c7
Stephen <steve.wiggins7@gmail.com>
2016-03-13 08:52:39 -0400

    add the constants changes

--- App/app/scripts/services/myconstants.js
+++ App/app/scripts/services/myconstants.js
@@ -8,4 +8,11 @@
  * Constant in the appApp.
  */
 angular.module('appApp')
-  .constant('myConstants', 42);
+  
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+})
+ 
+.constant('API_ENDPOINT', {
+  url: 'http://127.0.0.1:1337/api' //Sails API URL
+});
