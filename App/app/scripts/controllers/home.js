a1a50468e0705bdd43a7bd175cc88e26ed0ed61f
Stephen <steve.wiggins7@gmail.com>
2016-03-20 03:09:04 -0400

    basic data fetching works

--- App/app/scripts/controllers/home.js
+++ App/app/scripts/controllers/home.js
@@ -8,6 +8,57 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-  .controller('HomeCtrl', function () {
-    
-  });
+    .controller('HomeCtrl', function ($http, $scope, API_ENDPOINT) {
+        //get the data
+        $http({
+            method: 'GET',
+            url: 'http://localhost:1337/ipaddress'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.ipaddressData = response.data;
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.ipaddressData = "failed";
+        });
+        //post
+        $http({
+            method: 'GET',
+            url: 'http://localhost:1337/ipaddress'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.ipaddressData = response.data;
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.ipaddressData = "failed";
+        });
+        //put
+        $http({
+            method: 'GET',
+            url: 'http://localhost:1337/ipaddress'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.ipaddressData = response.data;
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.ipaddressData = "failed";
+        });
+        //delete
+        $http({
+            method: 'GET',
+            url: 'http://localhost:1337/ipaddress'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.ipaddressData = response.data;
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.ipaddressData = "failed";
+        });
+    });
