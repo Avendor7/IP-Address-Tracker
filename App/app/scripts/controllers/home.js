89095246f5870500102a0568afcf3a45134366f4
Stephen <steve.wiggins7@gmail.com>
2016-03-23 08:17:24 -0400

    forgot to save a file

--- App/app/scripts/controllers/home.js
+++ App/app/scripts/controllers/home.js
@@ -120,3 +120,18 @@ angular.module('appApp')
 
         };
     });
+angular.module('appAppo').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
