98e2c7289ba1a3aa0eca6c63615e9a14a5b0488a
Stephen <steve.wiggins7@gmail.com>
2016-05-06 03:49:05 -0400

    new post stuff is broken

--- App/app/scripts/controllers/newpost.js
+++ App/app/scripts/controllers/newpost.js
@@ -8,23 +8,25 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-  .controller('NewpostCtrl', function ($scope,blogFactory) {
-     
-        
-        //write put request to database factory
-        blogFactory.post($scope.newPostHTML)
-          .success(function () {
-            //TODO: have factory return the id so $state can be redirected
-            
-          })
-          .error(function (response, status) {
-            switch (status) {
-              case 404:
-                console.log('404 server not found');
-                break;
-              default:
-                console.log('General Error');
-                break;
-            }
-          });
+  .controller('NewpostCtrl', function ($scope, blogFactory) {
+    
+    function makePost() {
+      //write put request to database factory
+      console.log("clicked");
+      blogFactory.post($scope.newPost)
+        .success(function () {
+          //TODO: have factory return the id so $state can be redirected
+          console.log("It Worked... finally");
+        })
+        .error(function (response, status) {
+          switch (status) {
+            case 404:
+              console.log('404 server not found');
+              break;
+            default:
+              console.log('General Error');
+              break;
+          }
+        });
+    }
   });
