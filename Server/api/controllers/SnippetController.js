2833bdb217a11778abf4991f8977ddb6c3d2bbf0
Stephen <steve.wiggins7@gmail.com>
2016-05-13 05:05:32 -0400

    create snippet API, modify settings maybe?

---
+++ Server/api/controllers/SnippetController.js
@@ -0,0 +1,11 @@
+/**
+ * SnippetController
+ *
+ * @description :: Server-side logic for managing snippets
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
