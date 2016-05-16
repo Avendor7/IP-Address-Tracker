105c691631d6ba828b13c1a2b834bb3341144f38
Stephen <steve.wiggins7@gmail.com>
2016-05-16 02:22:47 -0400

    delete snippet now working

--- App/app/scripts/controllers/snippet.js
+++ App/app/scripts/controllers/snippet.js
@@ -8,7 +8,7 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-    .controller('SnippetCtrl', function ($scope, snippetFactory, $stateParams) {
+    .controller('SnippetCtrl', function ($scope, snippetFactory, $stateParams, $state) {
         //get current snippet ID from URL
         var currentId = $stateParams.id;
         //query the database for the snippet data
@@ -17,14 +17,35 @@ angular.module('appApp')
                 //on success, load the editor and the snippet into the editor
                 $scope.snippet = response;
 
-                
                 //when save button is pressed
                 $scope.updateSnippet = function () {
-                    //write put request to database factory
+                    //write put request to snippet factory
                     snippetFactory.put($scope.snippet)
                         .success(function () {
                             //TODO: have factory return the id so $state can be redirected
                             console.log("It Worked... finally");
+                        })
+                        .error(function (response, status) {
+                            switch (status) {
+                                case 404:
+                                    console.log('404 server not found');
+                                    break;
+                                default:
+                                    console.log('General Error');
+                                    break;
+                            }
+                        });
+                };
+                
+                $scope.deleteSnippet = function () {
+                    //send the delete request to snippet factory
+                    
+                    snippetFactory.delete(currentId)
+                        .success(function (response,status) {
+                            //TODO: have factory return the id so $state can be redirected
+                            $state.go("user.viewSnippets");
+                            console.log(status);
+                            console.log("Boom, deleted");
                         })
                         .error(function (response, status) {
                             switch (status) {
