91f1d5b3f062320f7d06fc4254317088219c320f
Stephen <steve.wiggins7@gmail.com>
2016-06-05 01:32:37 -0400

    created api for weather, preparing for 1.0 release of server (sails)

---
+++ Server/api/controllers/WeatherController.js
@@ -0,0 +1,11 @@
+/**
+ * WeatherController
+ *
+ * @description :: Server-side logic for managing weathers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
